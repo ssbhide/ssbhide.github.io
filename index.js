031e0b474072ab42af819fb03e716bb990191a4a
Sarang Bhide <154283927+ssbhide@users.noreply.github.com>
2024-12-17 13:48:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-//index.js
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll("nav ul li a");
 
-const hamburger = document.getElementById('hamburger'); 
-const menu = document.querySelector('.menu'); 
-
-hamburger.addEventListener('click', function () { 
-    const hamIcon = this.querySelector('.hamburger-icon'); 
-    const crossIcon = this.querySelector('.cross-icon'); 
-    if (hamIcon.style.display === "none") { 
-        hamIcon.style.display = "inline-block"
-        menu.style.display = "none"
-        crossIcon.style.display = "none"
-    } 
-    else { 
-        crossIcon.style.display = "inline-block"
-        hamIcon.style.display = "none"
-        menu.style.display = "block"
-    } 
+    links.forEach(link => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const target = document.querySelector(link.getAttribute("href"));
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
 });
